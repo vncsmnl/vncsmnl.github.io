0d5f2df4604985e82db61fd4eaad6cfbf0f17ef6
Vinícius Manoel <59481808+vncsmnl@users.noreply.github.com>
2023-08-02 17:04:31 -0300

    :rocket: Add: added js file

---
+++ script.js
@@ -0,0 +1,11 @@
+/* This JavaScript code is adding event listeners to the window object for the 'blur' and 'focus'
+events. */
+let docTitle = document.title
+
+window.addEventListener('blur', () => {
+    document.title = 'Ooops, ainda estou aqui!'
+})
+
+window.addEventListener('focus', () => {
+    document.title = docTitle
+})
